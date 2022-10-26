1d1244401d1726cd855d6444ba7e0764de851522
ryzensp1 <116732740+ryzensp1@users.noreply.github.com>
2022-10-27 01:48:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,9 +17,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID', '7460290'])
-API_HASH = environ['API_HASH', '40f25e810b628be54bcb44a58eb8d69e']
-BOT_TOKEN = environ['BOT_TOKEN', '2034149452:AAE_TDxppP1SIvTY2NxQO0UvJ9AYbpimR_4']
+API_ID = int(environ.get('API_ID', '7460290'))
+API_HASH = environ.get('API_HASH', '40f25e810b628be54bcb44a58eb8d69e')
+BOT_TOKEN = environ.get('BOT_TOKEN', '2034149452:AAE_TDxppP1SIvTY2NxQO0UvJ9AYbpimR_4')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
